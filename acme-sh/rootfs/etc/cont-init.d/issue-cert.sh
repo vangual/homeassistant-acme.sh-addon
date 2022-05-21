69d47a2bf3ee06b489663e79fdbaa92df48ca4af
Avi Miller <me@dje.li>
2022-05-21 15:31:08 +1000

    Remove debugging
    Signed-off-by: Avi Miller <me@dje.li>

--- acme-sh/rootfs/etc/cont-init.d/issue-cert.sh
+++ acme-sh/rootfs/etc/cont-init.d/issue-cert.sh
@@ -16,7 +16,6 @@ DNS_ENV_VARS=$(jq --raw-output '.dnsenvvars | map("export \(.name)='\''\(.value)
 KEY_LENGTH=$(bashio::config 'keylength')
 FULLCHAIN_FILE=$(bashio::config 'fullchainfile')
 KEY_FILE=$(bashio::config 'keyfile')
-DEBUG_LEVEL=$(bashio::config 'debug')
 
 # shellcheck source=/dev/null
 source <(echo "$DNS_ENV_VARS");
@@ -32,7 +31,6 @@ function issue {
     acme.sh --issue --domain "$DOMAIN" \
         --keylength "$KEY_LENGTH" \
         --dns "$DNS_PROVIDER" \
-        --debug "$DEBUG_LEVEL" \
         || { ret=$?; [ $ret -eq ${RENEW_SKIP} ] && return 0 || return $ret ;}
 }
 
@@ -40,10 +38,9 @@ issue
 
 bashio::log.info "Installing private key to /ssl/$KEY_FILE and certificate to /ssl/$FULLCHAIN_FILE"
 ECC_ARG=$( [[ ${KEY_LENGTH} == ec-* ]] && echo '--ecc' || echo '' )
-[ ! -d "/ssl/${DOMAIN}/" ] && mkdir -p "/ssl/${DOMAIN}/"
+
 acme.sh --install-cert --domain "$DOMAIN" "$ECC_ARG" \
         --key-file       "/ssl/$KEY_FILE" \
-        --fullchain-file "/ssl/$FULLCHAIN_FILE" \
-        --debug "$DEBUG_LEVEL"
+        --fullchain-file "/ssl/$FULLCHAIN_FILE"
 
 bashio::log.info "Inital configuration complete."
